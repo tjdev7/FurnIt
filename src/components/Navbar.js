48ce5816c8de31d63018b9be9a2e0922b8d008b8
Toughee <MJ1menez@protonmail.com>
2020-09-26 23:30:18 -0500

    FurnIt/009/UI_Drafting_1 - Made initial UI elements and components for app

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Navbar () {
+	return (
+
+		// Logo
+		// App title
+		// Item count
+		// Cart
+
+	);
+}
+
+const User () {
+	
+}
+
+const AppLogo () {
+	return (
+
+	);
+}
+
+const AppTitle() {
+	return(
+
+	);
+}
+
+const ItemCount() {
+	return (
+
+	);
+}
+
+const cart {
+	return (
+		{item amount:0}
+	);
+}
