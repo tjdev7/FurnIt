e21fe0a4b5fcce5d921f742a5b4c92fd9fb5095c
Toughee <MJ1menez@protonmail.com>
2020-10-21 22:58:58 -0500

    FurnIt/013/Catalog_layout_test_02 - Improved component infrasctuture and UI layout

--- src/App.js
+++ src/App.js
@@ -6,39 +6,54 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span>
+        <span className="appname">
           FurnIt 
         </span>
         <span className = "MenuList">
           Items | Catalog
         </span>
         <span className = "Cart">
-          <i class='fas fa-shopping-cart'></i> (0) Cart
+          <i className='fas fa-shopping-cart'></i> (0) Cart
         </span>
       </header>
 
       <div className="ItemDisplay">
         <img className="ImgStyle" src={TestImg} alt="TestImg" />
         <span className="ItemName"> Item</span>
-        <br/>
-        <span className="DescStyle">Desc |</span>
-        <span className="PriceStyle"> $Price</span>
+        <br />
+        <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+        <br />
+        Etiam volutpat dolor id maximus rutrum. 
+        <br />
+        Donec sit amet faucibus augue. </span>
+        <br />
+        <span className="PriceStyle"> $000.00</span>
       </div>
 
       <div className="ItemDisplay">
         <img className="ImgStyle" src={TestImg} alt="TestImg" />
         <span className="ItemName"> Item</span>
-        <br/>
-        <span className="DescStyle">Desc |</span>
-        <span className="PriceStyle"> $Price</span>
+        <br />
+        <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+        <br />
+        Etiam volutpat dolor id maximus rutrum. 
+        <br />
+        Donec sit amet faucibus augue. </span>
+        <br />
+        <span className="PriceStyle"> $00.00</span>
       </div>
 
       <div className="ItemDisplay">
         <img className="ImgStyle" src={TestImg} alt="TestImg" />
         <span className="ItemName"> Item</span>
-        <br/>
-        <span className="DescStyle">Desc |</span>
-        <span className="PriceStyle"> $Price</span>
+        <br />
+        <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+        <br />
+        Etiam volutpat dolor id maximus rutrum. 
+        <br />
+        Donec sit amet faucibus augue. </span>
+        <br />
+        <span className="PriceStyle"> $00.00</span>
       </div>
 
     </div>
