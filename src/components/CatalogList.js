66301025351ddca878f2415165d3f89df0f87d6c
Toughee <MJ1menez@protonmail.com>
2020-10-28 10:27:01 -0500

    FurnIt/017/Component-refactoring_01 - Removed unnecesary components and files - Refactored code for components

--- src/components/CatalogList.js
+++ src/components/CatalogList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TestImg from './TestImg.png';
-import testimg2 from './testimg2.jpg';
-import testimg5 from './testimg5.jpg';
-import testimg8 from './testimg8.jpg';
+import TestImg from './img/TestImg.png';
+import testimg2 from './img/testimg2.jpg';
+import testimg3 from './img/testimg3.jpg';
+import testimg4 from './img/testimg4.jpg';
+import testimg5 from './img/testimg5.jpg';
+import testimg6 from './img/testimg6.jpg';
+import testimg8 from './img/testimg8.jpg';
+import testimg9 from './img/testimg9.jpg';
 
 function CatalogList() {catalog} {
 	return (
@@ -15,80 +19,50 @@ function CatalogList() {catalog} {
 var CatalogDisplay = [
 	{
 		id: 0,
-		name: "Dinner set 1",
+		name: "Dresser Set",
 		description: "",
 		price: 
 	},
 	{
-		id: 0,
-		name: "dinner set 2",
+		id: 1,
+		name: "Persian Rug",
 		description: "",
 		price:
 	},
 	{
-		id: 0,
+		id: 2,
 		name: "dinner set 3",
 		description: "",
 		price:
 	},
 	{
-		id: 0,
-		name: "Sofa 1",
+		id: 3,
+		name: "Minimalist lamp",
 		description: "",
 		price:
 	},
 	{
-		id: 0,
-		name: "Sofa 2",
+		id: 4,
+		name: "Outdoor Sofa",
 		description: "",
 		price:
 	},
 	{
-		id: 0,
+		id: 5,
 		name: "mini sofa 1",
 		description: "",
 		price:
 	},
 	{
-		id: 0,
-		name: "Plate set 1",
+		id: 6,
+		name: "Sofa set",
 		description: "",
 		price:
 	},
 	{
-		id: 0,
-		name: "Plate set 2",
+		id: 7,
+		name: "Plate Set",
 		description: "",
 		price:
-	},
-	{
-		id: 0,
-		name: "Dresser set 1",
-		description: "",
-		price:
-	},
-	{
-		id: 0,
-		name: "Dresser set 2",
-		description: "",
-		price:
-	},
-	{
-		id: 0,
-		name: "Patio set 1",
-		description: "",
-		price:
-	},
-	{
-		id: 0,
-		name: "Patio set 2",
-		description: "",
-		price:
-	},
-	{
-		id: 0,
-		name: "Patio set 3",
-		description: "",
-		price:
-	},
+	}
 ]
--- src/index.js
+++ src/index.js
@@ -1,14 +1,173 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import TestImg from './img/TestImg.png';
+import testimg2 from './img/testimg2.jpg';
+import testimg3 from './img/testimg3.jpg';
+import testimg4 from './img/testimg4.jpg';
+import testimg5 from './img/testimg5.jpg';
+import testimg6 from './img/testimg6.jpg';
+import testimg7 from './img/testimg7.jpg';
+import testimg8 from './img/testimg8.jpg';
+import testimg9 from './img/testimg9.jpg';
+import testimg10 from './img/testimg10.jpg';
+import './App.css';
+import './bootstrap.css';
+
+function CatalogDisplay() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span className="appname">
+          FurnIt 
+        </span>
+        <span className = "MenuList">
+           Catalog
+        </span>
+        <span className = "Cart">
+          <i className='fas fa-shopping-cart'></i> (0) Cart
+        </span>
+      </header>
+      <main className="bannerdefault"></main>
+      <span className="container">
+      <div className="row">
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg2} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Dresser Set</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $000.00</span>
+          <button className="CartAdd"> Add to cart</button>
+        </div>
+
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg3} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Persian Rug</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $00.00</span>
+          <button className="CartAdd"> Add to cart</button>
+        </div>
+
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg4} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Minimalist lamp</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $00.00</span>
+          <button className="CartAdd"> Add to cart</button>
+          
+        </div>
+        </div>
+        </span>
+
+        <span className="container">
+      <div className="row">
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg5} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Sofa Chair</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $000.00</span>
+          <button className="CartAdd"> Add to cart</button>
+        </div>
+
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg6} alt="TestImg" />
+          <br />
+          <span className="ItemName">Minimalist chair</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $00.00</span>
+          <button className="CartAdd"> Add to cart</button>
+
+        </div>
+
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg7} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Item</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $00.00</span>
+          <button className="CartAdd"> Add to cart</button>
+        </div>
+        </div>
+        </span>
+
+        <span className="container">
+      <div className="row">
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg8} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Sofa</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $000.00</span>
+          <button className="CartAdd"> Add to cart</button>
+        </div>
+
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg9} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Plate set</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $00.00</span>
+          <button className="CartAdd"> Add to cart</button>
+
+        </div>
+
+        <div className="ItemDisplay col">
+          <img className="ImgStyle" src={testimg10} alt="TestImg" />
+          <br />
+          <span className="ItemName"> Dinner Set</span>
+          <br />
+          <span className="DescStyle">  In vel arcu volutpat, tempor lorem eget, sollicitudin felis. 
+          Etiam volutpat dolor id maximus rutrum. 
+          Donec sit amet faucibus augue. </span>
+          <br />
+          <span className="PriceStyle"> $00.00</span>
+          <button className="CartAdd"> Add to cart</button>
+        </div>
+        </div>
+        </span>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CatalogDisplay />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
