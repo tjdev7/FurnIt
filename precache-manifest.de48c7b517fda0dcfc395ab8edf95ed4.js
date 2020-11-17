23169490540362a9858752d7a62e17b70408ed6b
Toughee <Jimernez.io@gmail.com>
2020-11-17 17:18:51 -0600

    Updates

---
+++ precache-manifest.de48c7b517fda0dcfc395ab8edf95ed4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b960c9e661b4260aea5897b43eba083",
+    "url": "/FurnIt/index.html"
+  },
+  {
+    "revision": "da857a12f70069f1f4de",
+    "url": "/FurnIt/static/css/main.a859786b.chunk.css"
+  },
+  {
+    "revision": "c15d578064aa77dcf249",
+    "url": "/FurnIt/static/js/2.35ed8d46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FurnIt/static/js/2.35ed8d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da857a12f70069f1f4de",
+    "url": "/FurnIt/static/js/main.bca82769.chunk.js"
+  },
+  {
+    "revision": "1cbe926df6844d277269",
+    "url": "/FurnIt/static/js/runtime-main.5e66cdad.js"
+  },
+  {
+    "revision": "7124005b3c2b6616829c235a4407d4d8",
+    "url": "/FurnIt/static/media/testimg10.7124005b.jpg"
+  },
+  {
+    "revision": "471d905e60a4909d48ab0cb3fb0ba82e",
+    "url": "/FurnIt/static/media/testimg2.471d905e.jpg"
+  },
+  {
+    "revision": "6b8e01444c2155a2709f2beea35002da",
+    "url": "/FurnIt/static/media/testimg3.6b8e0144.jpg"
+  },
+  {
+    "revision": "458d418c69990b794996875c7e02e682",
+    "url": "/FurnIt/static/media/testimg4.458d418c.jpg"
+  },
+  {
+    "revision": "68f2ffb5412a8003adb5d2ef786986fd",
+    "url": "/FurnIt/static/media/testimg5.68f2ffb5.jpg"
+  },
+  {
+    "revision": "b2f929e6c74f9914341562a097b53d73",
+    "url": "/FurnIt/static/media/testimg6.b2f929e6.jpg"
+  },
+  {
+    "revision": "f8d1d82ec9e5bc3a9665ee85b52606ff",
+    "url": "/FurnIt/static/media/testimg7.f8d1d82e.jpg"
+  },
+  {
+    "revision": "dbdcec6f1c4e416d52dccf7093c59b53",
+    "url": "/FurnIt/static/media/testimg8.dbdcec6f.jpg"
+  },
+  {
+    "revision": "7d14a05bc934fe7605c8ec7b533bd074",
+    "url": "/FurnIt/static/media/testimg9.7d14a05b.jpg"
+  }
+]);
